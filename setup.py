7f4f0d4bcebc884d74e9e9686a207adf2ef2e48e
mrrushikeshdaund <rushikesh.daund@yahoo.com>
2023-03-25 17:54:57 +0530

    Adding files

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup,find_packages
+
+setup(name="census-income",version="0.0.1",author="Rushikesh Daund",author_email="errushikeshdaund@gmail.com",packages=find_packages(),install_requires =["pandas","numpy","flask"])
